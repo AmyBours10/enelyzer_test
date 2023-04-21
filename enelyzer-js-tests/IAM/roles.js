688b64bd39da5f8409debeadf42f3adcbb832fe8
Amy Bours <amy@trust1team.com>
2023-04-21 09:45:18 +0200

    no message

---
+++ enelyzer-js-tests/IAM/roles.js
@@ -0,0 +1,185 @@
+var idpurl = "https://staging-gateway.enprove.be";
+var apiurl = "https://staging-gateway.enprove.be";
+var gatewayurl = "https://staging-gateway.enprove.be";
+var orgid = "00e28801-3fd2-4841-98cc-9bdcef094774";
+var maintenancedoc = "b3a5951e-c1c9-4367-ab72-41a72071bf98";
+
+//create role
+var myHeaders = new Headers();
+myHeaders.append("Authorization", `Bearer ${accessToken}`);
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "organisationId": "00e28801-3fd2-4841-98cc-9bdcef094774",
+  "name": "Energy_Manager_test5",
+  "friendlyName": "Energy Manager test",
+  "permissions": [
+    "READ_ENERGY_EFFICIENCY",
+    "MUTATE_ENERGY_EFFICIENCY"
+  ]
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/organisations/${orgid}/roles", requestOptions, idpurl, orgid)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+// search roles
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${accessToken}`);
+  myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "from": 0,
+  "size": 20,
+  "permissions": [
+    "EDIT_ACTIONNABLE_MOBILE_TASKS"
+  ]
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/organisations/${orgid}/roles/search", requestOptions, idpurl, orgid)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+//update role
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${accessToken}`);
+  myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "name": "Energy Manager",
+  "permissions": [
+    "READ_ENERGY_EFFICIENCY",
+    "MUTATE_ENERGY_EFFICIENCY"
+  ]
+});
+
+var requestOptions = {
+  method: 'PUT',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/organisations/${orgid}/roles/a02c3675-be3c-42aa-832e-b25472f3aaca", requestOptions, idpurl, orgid)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+//get role
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${accessToken}`);
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/organisations/${orgid}/roles/a02c3675-be3c-42aa-832e-b25472f3aaca", requestOptions, idpurl, orgid)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+//add permission to role
+  var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "permission": "READ_AUDITS_PROJECTS"
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/roles/a02c3675-be3c-42aa-832e-b25472f3aaca/permissions", requestOptions, idpurl)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+//remove permission from role
+  var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "permission": "READ_AUDITS_PROJECTS"
+});
+
+var requestOptions = {
+  method: 'DELETE',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/roles/a02c3675-be3c-42aa-832e-b25472f3aaca/permissions", requestOptions, idpurl)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+//disable role
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${accessToken}`);
+  myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "newRole": "92508ed7-8543-46dd-bfe5-64101fde533b"
+});
+
+var requestOptions = {
+  method: 'DELETE',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/organisations/${orgid}/roles/92508ed7-8543-46dd-bfe5-64101fde533b", requestOptions, idpurl, orgid)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+// enable role
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${accessToken}`);
+  myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "permission": "READ_AUDITS_PROJECTS"
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/organisations/${orgid}/roles/92508ed7-8543-46dd-bfe5-64101fde533b/enable", requestOptions, idpurl, orgid)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
