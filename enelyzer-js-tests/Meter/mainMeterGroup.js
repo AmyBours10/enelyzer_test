9948553eb9a1de6c824c217efd00a28a080b1e9b
Amy Bours <amy@trust1team.com>
2023-04-24 11:03:03 +0200

    main meter group

--- enelyzer-js-tests/Meter/mainMeterGroup.js
+++ enelyzer-js-tests/Meter/mainMeterGroup.js
@@ -2,38 +2,20 @@ var idpurl = "https://staging-gateway.enprove.be";
 var apiurl = "https://staging-gateway.enprove.be";
 var gatewayurl = "https://staging-gateway.enprove.be";
 var orgid = "00e28801-3fd2-4841-98cc-9bdcef094774";
-//get energy source
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch("${idpurl}/v1/meter-energy-sources/:id", requestOptions, idpurl)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+var metergroupid = "399f8cb6-b2ae-4960-8bfc-805464c72e57";
 
-
-// get energy source meter types
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
-
-fetch("${idpurl}/v1/meter-energy-sources/view/types", requestOptions, idpurl)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-
-
-  //list meter energy source
+// create main meter group
 var myHeaders = new Headers();
+myHeaders.append("Authorization", `Bearer ${accessToken}`);
 myHeaders.append("Content-Type", "application/json");
 
 var raw = JSON.stringify({
-  "pageFrom": 0,
-  "pageSize": 10
+  "organisationId": "00e28801-3fd2-4841-98cc-9bdcef094774",
+  "facilityId": "1aab41af-e2a0-44dd-9715-b884d0c23e7f",
+  "energySourceId": "",
+  "description": "test group",
+  "name": "test_source",
+  "friendlyName": "test source"
 });
 
 var requestOptions = {
@@ -43,16 +25,21 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch("${idpurl}/v1/meter-energy-sources/view", requestOptions, idpurl)
+fetch("${idpurl}/v1/organisations/${orgid}/main-meters-groups", requestOptions, idpurl, orgid)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
 
-  //intialise meter energy sources
-  var myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${accessToken}`);
 
-var raw = "";
+//main meter groups tree
+var myHeaders = new Headers();
+myHeaders.append("Authorization", `Bearer ${accessToken}`);
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "organisationId": "00e28801-3fd2-4841-98cc-9bdcef094774",
+  "facilityId": "1aab41af-e2a0-44dd-9715-b884d0c23e7f"
+});
 
 var requestOptions = {
   method: 'POST',
@@ -61,7 +48,113 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch("${idpurl}/v1/utility/energy-sources/init", requestOptions, idpurl)
+fetch("${idpurl}/v1/organisations/${orgid}/main-meters-groups/tree", requestOptions, idpurl, orgid)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+//search main meter groups
+var myHeaders = new Headers();
+myHeaders.append("Authorization", `Bearer ${accessToken}`);
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "organisationId": "00e28801-3fd2-4841-98cc-9bdcef094774",
+  "facilityId": "1aab41af-e2a0-44dd-9715-b884d0c23e7f"
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/organisations/${orgid}/main-meters-groups/view", requestOptions, idpurl, orgid)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+//update main meter groups
+var myHeaders = new Headers();
+myHeaders.append("Authorization", `Bearer ${accessToken}`);
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "organisationId": "00e28801-3fd2-4841-98cc-9bdcef094774",
+  "facilityId": "1aab41af-e2a0-44dd-9715-b884d0c23e7f",
+  "energySourceId": "",
+  "description": "test group updated",
+  "name": "test_source updated",
+  "friendlyName": "test source updated"
+});
+
+var requestOptions = {
+  method: 'PUT',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/organisations/${orgid}/main-meters-groups/${metergroupid}", requestOptions, idpurl, orgid, metergroupid)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+//get main meter group
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${accessToken}`);
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({});
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/organisations/${orgid}/main-meters-groups/${metergroupid}", requestOptions, idpurl, orgid, metergroupid)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+//disable main meter group
+
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${accessToken}`);
+
+var requestOptions = {
+  method: 'DELETE',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/organisations/${orgid}/main-meters-groups/${metergroupid}", requestOptions, idpurl, orgid, metergroupid)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+
+//enable main meter group
+var myHeaders = new Headers();
+myHeaders.append("Authorization", `Bearer ${accessToken}`);
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("${idpurl}/v1/organisations/${orgid}/main-meters-groups/${metergroupid}/enable", requestOptions, idpurl, orgid, metergroupid)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
